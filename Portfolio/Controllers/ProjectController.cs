3b6d31b326220095c1440c06572113e2daccf333
ZiyaMammadov1 <ziyaam@code.edu.az>
2024-01-09 23:41:24 +0400

    Add project files.

---
+++ Portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Cabinet()
+        {
+            return View();
+        }
+    }
+}
