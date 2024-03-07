f964f10c73412672c92bc3cee4cf8fce0f262e11
ZiyaMammadov1 <87971362+ZiyaMammadov1@users.noreply.github.com>
2024-03-07 16:45:26 +0400

    fix some bugs

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -15,5 +15,15 @@ namespace Portfolio.Controllers
         {
             return View();
         }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
     }
 }
